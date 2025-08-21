e0e4f27ff07a0e7412dc1a08369f3df1d742dc76
Yash Kambaria <yashkambaria245@gmail.com>
2025-08-21 20:20:20 +0530

    Stickiness applied

--- lib/screen/home.dart
+++ lib/screen/home.dart
@@ -8,7 +8,6 @@ import 'package:zomato_clone/widgets/zomato_gold_widget.dart';
 import '../module/home_item_card.dart';
 import '../widgets/top_app_bar_widget.dart';
 
-
 class Home extends StatefulWidget {
   const Home({super.key});
 
@@ -25,48 +24,124 @@ class _HomeState extends State<Home> {
       "4.5",
       "20",
       "25",
-      "Faasos - Wraps and rolls"
+      "Faasos - Wraps and rolls",
     ),
-    HomeItemCard('assets/Noodles.avif', "50% off", "Faasos - Wraps and rolls ", "4.5", "20", "25","Chinese King"),
-    HomeItemCard('assets/Cake.avif', "50% off", "Cake", "4.5", "20", "25","Faasos - Wraps and rolls"),
-    HomeItemCard('assets/Cake.avif', "50% off", "Cake", "4.5", "20", "25","Faasos - Wraps and rolls"),
-    HomeItemCard('assets/Cake.avif', "50% off", "Cake", "4.5", "20", "25","Monginis"),
-    HomeItemCard('assets/Cake.avif', "50% off", "Cake", "4.5", "20", "25","Faasos - Wraps and rolls"),
-    HomeItemCard('assets/Cake.avif', "50% off", "Cake", "4.5", "20", "25","Faasos - Wraps and rolls"),
-    HomeItemCard('assets/Cake.avif', "50% off", "Cake", "4.5", "20", "25","Faasos - Wraps and rolls"),
+    HomeItemCard(
+      'assets/Noodles.avif',
+      "50% off",
+      "Faasos - Wraps and rolls ",
+      "4.5",
+      "20",
+      "25",
+      "Chinese King",
+    ),
+    HomeItemCard(
+      'assets/Cake.avif',
+      "50% off",
+      "Cake",
+      "4.5",
+      "20",
+      "25",
+      "Faasos - Wraps and rolls",
+    ),
+    HomeItemCard(
+      'assets/Cake.avif',
+      "50% off",
+      "Cake",
+      "4.5",
+      "20",
+      "25",
+      "Faasos - Wraps and rolls",
+    ),
+    HomeItemCard(
+      'assets/Cake.avif',
+      "50% off",
+      "Cake",
+      "4.5",
+      "20",
+      "25",
+      "Monginis",
+    ),
+    HomeItemCard(
+      'assets/Cake.avif',
+      "50% off",
+      "Cake",
+      "4.5",
+      "20",
+      "25",
+      "Faasos - Wraps and rolls",
+    ),
+    HomeItemCard(
+      'assets/Cake.avif',
+      "50% off",
+      "Cake",
+      "4.5",
+      "20",
+      "25",
+      "Faasos - Wraps and rolls",
+    ),
+    HomeItemCard(
+      'assets/Cake.avif',
+      "50% off",
+      "Cake",
+      "4.5",
+      "20",
+      "25",
+      "Faasos - Wraps and rolls",
+    ),
   ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color.fromARGB(255,24,23,28),
-      body: ListView(
-        children: [
-          TopAppBarWidget(),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              SearchBarWidget(),
-              ToggleModeWidget(),
-            ],
-          ),
-          ZomatoGoldWidget(),
-          Categoryscroller(),
-          FilterWidget(),
-          Container(
-            padding: EdgeInsets.all(20),
-            alignment: Alignment.centerLeft,
-            child: Text("RECOMMENDED FOR YOU",style:TextStyle(
-              color: Colors.grey,
+      backgroundColor: Color.fromARGB(255, 24, 23, 28),
+      body: SafeArea(
+        child: CustomScrollView(
+          slivers: [
+            SliverToBoxAdapter(child: TopAppBarWidget()),
+            SliverAppBar(
+              pinned: true,
+              backgroundColor: Color.fromARGB(255, 24, 23, 28),
+              elevation: 0,
+              automaticallyImplyLeading: false,
+              toolbarHeight: 70,
+              flexibleSpace: SizedBox(
+                height: 70,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [SearchBarWidget(), ToggleModeWidget()],
+                ),
+              ),
             ),
+            SliverToBoxAdapter(child: ZomatoGoldWidget()),
+            SliverAppBar(
+                pinned: true,
+                backgroundColor: Color.fromARGB(255, 24, 23, 28),
+                elevation: 0,
+                automaticallyImplyLeading: false,
+                toolbarHeight: 100,
+                flexibleSpace: Categoryscroller()
             ),
-          ),
-          RecommendedWidget(bucket1),
-          RecommendedWidget(bucket1),
-
-
-
-        ],
+            SliverToBoxAdapter(child: FilterWidget()),
+            SliverToBoxAdapter(
+              child: Container(
+                padding: EdgeInsets.all(20),
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  "RECOMMENDED FOR YOU",
+                  style: TextStyle(color: Colors.grey),
+                ),
+              ),
+            ),
+            SliverToBoxAdapter(child: RecommendedWidget(bucket1)),
+            SliverToBoxAdapter(child: RecommendedWidget(bucket1)),
+            SliverToBoxAdapter(child: RecommendedWidget(bucket1)),
+            SliverToBoxAdapter(child: RecommendedWidget(bucket1)),
+            SliverToBoxAdapter(child: RecommendedWidget(bucket1)),
+            SliverToBoxAdapter(child: RecommendedWidget(bucket1)),
+          ],
+        ),
       ),
     );
   }
 }
+
